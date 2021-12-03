171cc60fe5efa5bc90688d861b70e4ae882f0a9e
muhammadali8767 <muhammadali8767@gmail.com>
2021-12-03 18:23:53 +0500

    commit 1111

--- app/Console/Commands/Commit.php
+++ app/Console/Commands/Commit.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Bschmitt\Amqp\Publisher;
 
 class Commit extends Command
 {
@@ -23,36 +24,25 @@ class Commit extends Command
                 "INN" => "231456987"
             ]
         ]);
-        $queue = 'xt_in';
-        $exchange = 'common';
-        $routingKey = 'xt.request';
-        $replyTo = 'xt_in';
-        $timeout = 0;
 
-        $response = $this->queue_rpc(
-            $queue,
-            $exchange,
-            $routingKey,
-            $replyTo,
-            $message,
-            $timeout
-        );
-
+        $response = $this->queue_rpc('xt_in', $message, 0); // -> pong
         var_dump($response->getBody());
     }
 
-    private function queue_rpc(
-        $queue,
-        $exchange,
-        $routingKey,
-        $replyTo,
-        $message,
-        $timeout = 0
-    ) {
+    public function queue_rpc($queue, $message, $timeout = 0)
+    {
         /* @var Bschmitt\Amqp\Publisher $publisher */
         $publisher = app()->make('Bschmitt\Amqp\Publisher');
         $publisher->connect();
         $publisher->getConnection()->set_close_on_destruct();
+        $replyTo = $publisher->getChannel()->queue_declare(
+            'xt_in',
+            false,
+            false,
+            true,
+            true
+        );
+        $replyTo = $replyTo[0];
         $publisher->getChannel()->queue_declare(
             $queue,
             false,
@@ -60,10 +50,7 @@ class Commit extends Command
             false,
             false
         );
-
         $response = false;
-        $correlationId = uniqid();
-
         $publisher->getChannel()->basic_consume(
             $replyTo,
             '',
@@ -71,33 +58,24 @@ class Commit extends Command
             false,
             false,
             false,
-            function ($message) use (&$response, &$correlationId) {
-                if ($message->get('correlation_id') == $correlationId) {
-                    $response = $message;
-                }
+            function ($message) use (&$response) {
+                $response = $message;
             }
         );
-
-        $publisher->getChannel()->queue_bind(
-            $queue,
-            $exchange,
-            $routingKey
-        );
-
+        $publisher->getChannel()->queue_bind($queue, 'common', 'xt.request');
         $publisher->getChannel()->basic_publish(
             new \Bschmitt\Amqp\Message(
                 $message,
                 [
                     'user_id' => 'xt',
-                    'routing_key' => $routingKey,
+                    'routing_key' => 'xt.request',
                     'content_type' => 'application/json',
                     'delivery_mode' => 2,
-                    'correlation_id' => $correlationId,
                     'reply_to' => $replyTo,
                 ]
             ),
-            $exchange,
-            $routingKey
+            'common',
+            'xt.request'
         );
         $publisher->getChannel()->wait(null, false, $timeout);
         return $response;
